a47d40f4ee2ec26dbfeaf33065f7cf61fec58694
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-06-19 12:43:27 -0300

    feat: add new routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,21 @@ const routes = [
     name: 'home',
     component: () => import('@/views/HomeView.vue')
   },
+  {
+    path: '/coins',
+    name: 'coins',
+    component: () => import('@/views/CoinsView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('@/views/ContactView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/AboutView.vue')
+  },
 ]
 
 const router = createRouter({
