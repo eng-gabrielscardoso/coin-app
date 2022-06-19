0c28f9499d25099226e33857708ec70a268cb345
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-06-19 16:09:53 -0300

    refactor: update height rules

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <BaseNavbar />
-    <router-view class="min-h-screen flex justify-center items-center bg-gradient-to-t from-gray-700 via-gray-900 to-black text-white" />
+    <router-view class="min-h-screen w-full flex flex-col justify-center items-center bg-gradient-to-t from-gray-700 via-gray-900 to-black" />
     <BaseFooter />
   </div>
 </template>
