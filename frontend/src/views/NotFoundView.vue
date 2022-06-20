001c4068e1312ec7a749270b0f8a13fa7e716e62
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-06-19 22:55:37 -0300

    feat: add NotFound view

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="NotFoundView">
+    <div class="p-4 bg-white rounded text-center">
+      <span class="text-2xl text-black">
+        404 Page Not Found
+      </span>
+      <br>
+      <span>
+        Back to 
+        <router-link 
+          to="/"
+          class="text-blue-600 hover:text-blue-800 cursor-pointer"
+        >Home</router-link>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {  
+  name: "NotFoundView",
+  mounted() {
+    document.title = "About | CoinApp - Crypto Financial Market Online"
+  }
+}
+</script>
