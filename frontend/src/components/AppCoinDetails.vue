39abf99214daec1d29b2b1fda35fa0c570aca55b
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-06-20 09:45:55 -0300

    feat: separe coin details in single component

---
+++ frontend/src/components/AppCoinDetails.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mt-6 p-2 lg:px-6 bg-white rounded w-10/12 min-h-screen">
+    <div class="flex flex-col md:flex-row justify-between items-center w-full">
+      <div class="flex justify-center items-center md:w-5/12 my-4">
+        <img 
+          :src="coinDetails.image.small" 
+          :alt="coinDetails.name + ' image'" 
+          :title="coinDetails.name"
+        >
+        <h1 class="text-black text-6xl">
+          {{ this.coinDetails.name }}
+        </h1>
+      </div>
+      <div class="flex items-center justify-center w-full md:w-7/12 h-64 bg-gray-500 rounded">
+        <AlertIcon />
+        Charts in development
+      </div>
+    </div>
+    <div class="my-4 text-md">
+      <p 
+        v-html="coinDetails.description.en"
+        class="font-light"
+      ></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import AlertIcon from "@/assets/icons/AlertIcon.vue"
+
+export default {
+  name: "AppCoinDetails",
+  props: {
+    coinDetails: {
+      type: Object,
+      required: true,
+    }
+  },
+  components: {
+    AlertIcon,
+  },
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/CoinView.vue
+++ frontend/src/views/CoinView.vue
@@ -4,45 +4,22 @@
       v-if="showLoading" 
       class="relative"
     />
-    <div 
+    <AppCoinDetails 
       v-else
-      class="mt-6 p-2 lg:px-6 bg-white rounded w-10/12 min-h-screen"
-    >
-      <div class="flex flex-col md:flex-row justify-between items-center w-full">
-        <div class="flex justify-center items-center md:w-5/12 my-4">
-          <img 
-            :src="coinDetails.image.small" 
-            :alt="coinDetails.name + ' image'" 
-            :title="coinDetails.name"
-          >
-          <h1 class="text-black text-6xl">
-            {{ this.coinDetails.name }}
-          </h1>
-        </div>
-        <div class="flex items-center justify-center w-full md:w-7/12 h-64 bg-gray-500 rounded">
-          <AlertIcon />
-          Charts in development
-        </div>
-      </div>
-      <div class="my-4 text-md">
-        <p 
-          v-html="coinDetails.description.en"
-          class="font-light"
-        ></p>
-      </div>
-    </div>
+      :coinDetails="coinDetails"
+    />
   </div>
 </template>
 
 <script>
 import api from "@/plugins/api"
-import AlertIcon from "@/assets/icons/AlertIcon.vue"
+import AppCoinDetails from "@/components/AppCoinDetails.vue"
 import LoadingAnimation from "@/assets/animations/Loading.vue"
 
 export default {  
   name: "CoinView",
   components: {
-    AlertIcon,
+    AppCoinDetails,
     LoadingAnimation,
   },
   data() {
