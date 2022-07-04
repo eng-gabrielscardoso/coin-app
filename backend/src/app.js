2e41ad100b42c0f5d60dd6351091a51f85c8a4d8
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-07-03 22:54:51 -0300

    fix: fix env config for deployment in Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 const { PORT } = require('./config/vars')
 const app = require('./config/server')
 
-app.listen(PORT || 8082, async () => {
+app.listen(process.env.NODE_ENV !== 'prod' ? PORT : 8082, async () => {
   try {
-    await console.log(`Server running at port ${PORT}`)
+    await console.log(`Server running at port ${PORT || 8082}`)
   } catch(e) {
     await console.error(`Error running server: ${e}`)
   }
--- backend/src/config/vars.js
+++ backend/src/config/vars.js
@@ -1,9 +1,13 @@
 const path = require('path')
 
-require('dotenv-safe').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
-  examples: path.join(__dirname, '../../.env.example'),
-})
+if (process.env.NODE_ENV !== 'prod') {
+  require('dotenv-safe').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+    examples: path.join(__dirname, '../../.env.example'),
+    silent: true,
+    allowEmptyValues: true,
+  })
+}
 
 module.exports = {
   PORT: process.env.PORT,
