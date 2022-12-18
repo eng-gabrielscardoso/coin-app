ae9792ca7a6e522a8c24d78d3a4383058cc6db5a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-17 23:11:16 -0300

    feat[backend]: finish JSON response in fetchStatus

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,10 +3,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly author: string = 'Gabriel Santos Cardoso';
+
   constructor(private readonly configService: ConfigService) {}
 
   async fetchStatus() {
-    return 'Testing';
+    return JSON.stringify({
+      running: true,
+      author: this.author,
+    });
   }
 
   async getStatus() {
