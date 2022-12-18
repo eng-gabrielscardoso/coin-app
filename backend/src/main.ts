0cf22d10b9de1e9c09e3c0e1bfd29fb1fe5c3559
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-18 00:28:01 -0300

    fix[backend]: enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const port = config.get('PORT') || 3000;
+  await app.enableCors();
   await app.listen(port);
 }
 bootstrap();
