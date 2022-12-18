928492864552e9db3842d448d8fc541ee4e5662f
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-18 00:01:50 -0300

    fix[backend]: attempt to resolve vercel deploy

--- backend/core/validation.ts
+++ backend/core/validation.ts
@@ -2,4 +2,5 @@ import * as Joi from 'joi';
 
 export const validationSchema: Joi.ObjectSchema<any> = Joi.object({
   BASE_URL: Joi.string().default('https://api.coingecko.com/api/v3').required(),
+  PORT: Joi.number().default('8082'),
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const config = app.get(ConfigService);
+  const port = config.get('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
